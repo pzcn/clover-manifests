36984fe8e19c9cad441d1cacd6258e7a3faf6795
PedroZ <sdustpedro@gmail.com>
2025-02-09 20:43:26 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-git clone https://github.com/kyasu/android_device_xiaomi_sdm660-common.git device/xiaomi/sdm660-common -b lineage-22.1
-git clone https://github.com/kyasu/android_device_xiaomi_clover.git device/xiaomi/clover -b lineage-22.1
-git clone https://github.com/kyasu/android_kernel_xiaomi_sdm660.git kernel/xiaomi/sdm660 -b lineage-22.1
-git clone https://github.com/kyasu/android_vendor_xiaomi_sdm660-common.git vendor/xiaomi/sdm660-common -b lineage-22.1
-git clone https://github.com/kyasu/android_vendor_xiaomi_clover.git vendor/xiaomi/clover -b lineage-22.1
+git clone https://github.com/crdroidandroid/android_device_xiaomi_clover device/xiaomi/clover -b 15.0
+git clone https://github.com/crdroidandroid/android_device_xiaomi_sdm660-common.git device/xiaomi/sdm660-common -b 15.0
+git clone https://github.com/crdroidandroid/android_kernel_xiaomi_sdm660.git kernel/xiaomi/sdm660 -b 15.0
+git clone https://github.com/crdroidandroid/proprietary_vendor_xiaomi_clover.git vendor/xiaomi/clover -b 15.0
+git clone https://github.com/crdroidandroid/proprietary_vendor_xiaomi_sdm660-common.git vendor/xiaomi/sdm660-common -b 15.0
 
-. build/envsetup.sh
-lunch clover-userdebug
-mka bacon -j$(nproc --all)
+mkdir -p hardware/xiaomi
+cat << 'EOF' > hardware/xiaomi/Android.bp
+soong_namespace {
+}
+EOF
+source build/envsetup.sh
+lunch lineage_clover-ap3a-eng
+mka bacon
 
 
 
